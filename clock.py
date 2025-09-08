4d00ca69d9f0b7441a5240dfca4dfc19232eaf8b
Michal Lecicki <mlecicki@gmail.com>
2025-09-08 20:21:21 +0200

    Display date

--- clock.py
+++ clock.py
@@ -38,6 +38,7 @@ def run_clock():
 
     # Pick a font and size you like
     font_big = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 120)
+    font_small = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 40)
 
     try:
         while True:
@@ -56,14 +57,20 @@ def run_clock():
 
             # Center the text on screen
             w, h = draw.textsize(now, font=font_big)
-            x = (epd.width - w) // 2
-            y = (epd.height - h) // 2
+            time_x = (epd.width - w) // 2
+            time_y = (epd.height - h) // 2
 
-            draw.text((x, y), now, font=font_big, fill=0)
+            draw.text((time_x, time_y), now, font=font_big, fill=0)
+
+            dw, dh = draw.textsize(date, font=font_small)
+            date_x = (epd.width - dw) // 2
+            date_y = epd.height - dh - 20   # 20px margin from bottom
+            draw.text((date_x, date_y), date, font=font_small, fill=0)
 
             # Display
             epd.display(epd.getbuffer(image))
 
+            logging.info("Done. Going to sleep 60 seconds")
             # Wait until the next full minute
             time.sleep(60)
 
