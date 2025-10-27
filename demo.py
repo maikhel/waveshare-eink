5eb27f3edc2a5437cc7f3fe1e34a1d7a8cd072ef
maikhel <mlecicki@gmail.com>
2025-10-27 19:31:50 +0100

    Draw Steam Friends status

--- demo.py
+++ demo.py
@@ -8,6 +8,7 @@ def draw_demo():
     font = "/System/Library/Fonts/Supplemental/Arial Bold.ttf"
     image = drawing.draw_date_and_time(WIDTH, HEIGHT, font)
     drawing.draw_weather_info(image, WIDTH, HEIGHT, font)
+    drawing.draw_steam_friends(image, font)
 
     image.show()
     image.save("preview.png")
