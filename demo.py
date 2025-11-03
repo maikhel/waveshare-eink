6e2a224c8bbe78666347e5d887529f3bc2d470fb
maikhel <mlecicki@gmail.com>
2025-11-03 21:35:49 +0100

    Add a function to draw github info

--- demo.py
+++ demo.py
@@ -8,7 +8,8 @@ def draw_demo():
     font = "/System/Library/Fonts/Supplemental/Arial Bold.ttf"
     image = drawing.draw_date_and_time(WIDTH, HEIGHT, font)
     drawing.draw_weather_info(image, WIDTH, HEIGHT, font)
-    drawing.draw_steam_friends(image, font)
+    # drawing.draw_steam_friends(image, font)
+    drawing.draw_github_info(image, font)
 
     image.show()
     image.save("preview.png")
